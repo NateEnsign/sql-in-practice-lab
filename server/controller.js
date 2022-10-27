19248f92415771d040537ab7b406ac382d02e263
Nate Ensign <dixienate@gmail.com>
2022-10-26 19:10:41 -0600

    lab complete

--- server/controller.js
+++ server/controller.js
@@ -1,4 +1,17 @@
+require('dotenv').config()
+
+const Sequelize = require('sequelize')
+const CONNECTION_STRING = process.env.CONNECTION_STRING
+
 let nextEmp = 5
+
+const sequelize = new Sequelize(CONNECTION_STRING, {dialect: 'postgres',
+dialectOptions: {
+    ssl: {
+        rejectUnauthorized: false
+    }
+}
+})
 
 module.exports = {
     getUpcomingAppointments: (req, res) => {
@@ -8,7 +21,8 @@ module.exports = {
         join cc_employees e on e.emp_id = ea.emp_id
         join cc_users u on e.user_id = u.user_id
         where a.approved = true and a.completed = false
-        order by a.date desc;`)
+        order by a.date desc;
+        `)
             .then(dbRes => res.status(200).send(dbRes[0]))
             .catch(err => console.log(err))
     },
@@ -16,7 +30,9 @@ module.exports = {
     approveAppointment: (req, res) => {
         let {apptId} = req.body
     
-        sequelize.query(`*****YOUR CODE HERE*****
+        sequelize.query(`UPDATE cc_appointments
+                            SET approved = true
+                            WHERE appt_id = ${apptId};
         
         insert into cc_emp_appts (emp_id, appt_id)
         values (${nextEmp}, ${apptId}),
@@ -27,5 +43,65 @@ module.exports = {
                 nextEmp += 2
             })
             .catch(err => console.log(err))
+    },
+
+    getAllClients: (req, res) => {
+        sequelize.query(`
+            Select * FROM cc_clients AS c
+                JOIN cc_users AS u
+                ON c.user_id = u.user_id;
+        `)
+        .then((dbRes) => {
+            res.status(200).send(dbRes[0])
+        })
+        .catch((err) => {
+            console.log(err)
+            res.status(500).send('sequelize error')
+        })
+    },
+
+    getPendingAppointments: (req, res) => {
+        sequelize.query(`
+        SELECT * FROM cc_appointments
+        WHERE approved = false
+        ORDER BY date desc;
+        `)
+        .then((dbRes) => {
+            res.status(200).send(dbRes[0])
+            console.log(dbRes[0])
+        })
+        .catch((err) => {
+            console.log(err)
+            res.status(500).send('sequelize error')
+        })
+    },
+
+    getPastAppointments: (req, res) => {
+        sequelize.query(`select a.appt_id, a.date, a.service_type, a.notes, u.first_name, u.last_name 
+        from cc_appointments a
+        join cc_emp_appts ea on a.appt_id = ea.appt_id
+        join cc_employees e on e.emp_id = ea.emp_id
+        join cc_users u on e.user_id = u.user_id
+        where a.approved = true and a.completed = true
+        order by a.date desc;
+        `)
+        .then((dbRes) => {
+            res.status(200).send(dbRes[0])
+            console.log(dbRes[0])
+        })
+        .catch((err) => {
+            console.log(err)
+            res.status(500).send('sequelize error')
+        })
+    },
+
+    completeAppointment: (req, res) => {
+        let {apptId} = req.body
+
+        sequelize.query(`update cc_appointments set completed = true
+        where appt_id = ${apptId};`)
+            .then(dbRes => res.status(200).send(dbRes[0]))
+            .catch(err => console.log(err))
     }
+
 }
